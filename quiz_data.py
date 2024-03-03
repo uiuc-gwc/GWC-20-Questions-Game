758abc3a25fac425335440dfb3a9ec213e1c49f3
Hirish Chandrasekaran <hirish@ucsb.edu>
2024-03-02 22:47:10 -0600

    First Commit
    Contains 20 Questions UI

---
+++ quiz_data.py
@@ -0,0 +1,15 @@
+quiz_data = [{
+    "question": "What is the capital of France?",
+    "choices": ["Yes", "No"],
+}, {
+    "question": "What is the largest planet in our solar system?",
+    "choices": ["Yes", "No"],
+}, {
+    "question": "What is the chemical symbol for gold?",
+    "choices": ["Yes", "No"],
+}, {
+    "question": "Which country is known as the 'Land of the Rising Sun'?",
+    "choices": ["Yes", "No"],
+}
+             # Add more questions here
+             ]
